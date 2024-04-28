c2ff2a8ce164b11f13fc7a85e517bc2830379e90
Totom94 <tom.caldevilla--gonzalvez@efrei.net>
2024-04-28 16:26:53 +0200

    Meilleur affichage

--- Main.py
+++ Main.py
@@ -12,30 +12,29 @@ def main():
             print("\nContenu du fichier {} :".format(nom_fichier))
             print(contenu)
 
-            reponse = input("Voulez-vous afficher la matrice des coûts ? (oui/non) : ")
-            if reponse.lower() == "oui":
-                matrice_des_couts = lire_matrice_depuis_fichier(chemin_fichier)
-                print("Matrice des coûts :")
-                afficher_matrice_cout(matrice_des_couts[1:])
+        print("\n-------------------")
+        print("Calcul de la matrice des coûts :")
+        matrice_des_couts = lire_matrice_depuis_fichier(chemin_fichier)
+        afficher_matrice_cout(matrice_des_couts[1:])
 
-            reponse2 = input("Voulez-vous afficher la matrice de proposition de transport ? (oui/non) : ")
-            if reponse2.lower() == "oui":
-                matrice_des_prop = lire_matrice_depuis_fichier(chemin_fichier)
-                print("Matrice de proposition de transport (Pi et Cj) :")
-                afficher_matrice_proptrans(matrice_des_prop[1:])
+        print("\n-------------------")
+        print("Calcul de la matrice de proposition de transport (Pi et Cj) :")
+        matrice_des_prop = lire_matrice_depuis_fichier(chemin_fichier)
+        afficher_matrice_proptrans(matrice_des_prop[1:])
 
-            response3 = input("Voulez-vous résoudre le problème de transport avec la méthode du coin nord-ouest ? (oui/non) : ")
-            if response3.lower() == "oui":
-                matrice_des_prop = lire_matrice_depuis_fichier(chemin_fichier)
-                matrice_initiale = coin_nord_ouest(matrice_des_prop[1:])
-                afficher_matrice_transfert(matrice_initiale)
+        print("\n-------------------")
+        print("Application de la méthode du coin nord-ouest :")
+        matrice_des_prop = lire_matrice_depuis_fichier(chemin_fichier)
+        matrice_initiale = coin_nord_ouest(matrice_des_prop[1:])
+        afficher_matrice_transfert(matrice_initiale)
 
-            capacites = matrice_des_prop[-1]
-            reponse4 = input("Voulez-vous résoudre le problème de transport avec la méthode de Balas-Hammer ? (oui/non) : ")
-            if reponse4.lower() == "oui":
-                balas_hammer(matrice_des_couts[1:], matrice_des_prop[1:], capacites)
+        print("\n-------------------")
+        print("Application de la méthode de Balas-Hammer :")
+        capacites = matrice_des_prop[-1]
+        balas_hammer(matrice_des_couts[1:], matrice_des_prop[1:], capacites)
 
-            print("Fin du programme.")
+        print("\n-------------------")
+        print("Fin du programme.")
 
 if __name__ == "__main__":
     main()
